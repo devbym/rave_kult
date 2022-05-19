934f6edd40f5c537ff68a26f0c154bb419be40b3
Michiel (devbym) <33991635+devbym@users.noreply.github.com>
2022-05-19 10:17:59 +0200

    Added API and Location routes

--- routes.py
+++ routes.py
@@ -10,7 +10,28 @@ from database import sesh
 def home():
     return render_template("index.html",locations=[loc for loc in Location.query.all()],roles=list(UserRole))
 
+@app.route("/api", methods=["GET", "POST"])
+def api():
+    if request.method == "POST":
+        r = request.form
+        print(r)
+        new_user = Users(name=r.get("name"),email=r.get("email"),pwd=r.get('pass'),func=None,salt="1234")
+        sesh.add(new_user)
+        return jsonify(dict(status="OK",request=request.form.items()))
 
+    return jsonify(dict(status="OK"))
+
+@app.route("/api/location",methods=["POST"])
+def location():
+    if request.method == "POST":
+        r = request.form
+        print(r)
+        new_location = Location(r.get("locationName"),r.get("streetname"),r.get("streetnumber"),r.get("city"),r.get("country"))
+        sesh.add(new_location)
+        print(f"Location {new_location.name} added!")
+        locs = Location.query.all()
+        print([loc.name for loc in locs])
+        return jsonify(dict(request="OK",data=request.form))
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
